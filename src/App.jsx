50ffbe64790d7f0458841f5fd08cfd268ed3cb54
Aveenash <akb07070@gmail.com>
2022-08-10 15:31:02 +0500

    Added the footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Products from './components/Products';
 import {productData} from './components/Products/data';
 import {productDataTwo} from './components/Products/data';
 import Feature from './components/Feature';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
       <Products heading='Choose Your favourite' data={productData} />
       <Feature />
       <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
       </Router>
   );
 }
